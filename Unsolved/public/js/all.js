83a0dccb15d244b7834aa787901d4096e3d41b48
George Sophy <georgesophy@Georges-MacBook-Pro.local>
2018-08-30 21:13:02 -0400

    Coded all.js file

--- Unsolved/public/js/all.js
+++ Unsolved/public/js/all.js
@@ -6,12 +6,12 @@ $.get("/api", function(data) {
 for (var i=0; i <data.length; i++) {
     var petEntry = $("<div>");
     petEntry.addClass("entry");
-    petEntry.attr("id", "petID" + i);
+    petEntry.attr("id", "petDiv" + i);
     //change #pet-section to match what's in the HTML
     $("#pet-section").prepend(petEntry);
 
-
-
-}
-
+$("#petDiv" + i).append("<h2>" + data[i].petName + "</h2>");
+$("#petDiv" + i).append("<h3>Date: "+ data[i].createDate + "</h3>");
+$("#petDiv" + i).append("<h3>Journal Entry " + data[i].textbox + "</h3>");
+    }
 });
